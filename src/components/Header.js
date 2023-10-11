a0dfacb765cc898b4bfacb78a950697532ac16ed
HenaryKhuraijam <henarykhu123@gmial.com>
2023-10-11 22:16:18 +0530

    Add Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import SettingsIcon from '@mui/icons-material/Settings';
+import MicIcon from '@mui/icons-material/Mic';
+import { IconButton } from '@mui/material';
+import { useLocation, useNavigate } from 'react-router-dom';
 
-export const Header = () => {
+const Header = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+}
+
   return (
-    <div>
-      <h1>Welcome Henary</h1>
+    <div className='header'>
+    <div className='header__left'>
+    <img
+          src={}
+          alt=""
+        />
+    </div>
+      
+      <div className='header__center'>
+<IconButton>
+  {(location.pathname !== '/') && <ArrowBackIcon fontSize="medium" onClick={() => navigate('/')} />}
+</IconButton>
+      </div>
+
+      <div>
+        <IconButton>
+        <MicIcon />
+        </IconButton>
+        <IconButton>
+          <SettingsIcon />
+        </IconButton>
+      </div>
     </div>
   )
-}
+
+  export default Header;
+  
