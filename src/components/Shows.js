52f4f0b413f57f96d323c7824b4db456ea04111e
HenaryKhuraijam <henarykhu123@gmial.com>
2023-10-13 23:16:44 +0530

    update

--- src/components/Shows.js
+++ src/components/Shows.js
@@ -19,45 +19,31 @@ const Shows = () => {
   }, []);
 
   return (
-    <div
-      className="shows"
-    >
+    <div className="shows">
       <section
         className="shows_banner banner"
         style={{
           background: `linear-gradient(to top right, #1f1f1fff,
                 #b4b4b400) , url(${hellsing}) no-repeat center top`,
-          backgroundSize: '100% 100%',
+          backgroundSize: "100% 100%",
         }}
       >
         <div className="banner__intro">
           <article className="banner__intro__content">
-            <h1 className="banner__intro__title">
-              Welcome to MoviesWorld!
-            </h1>
-            <p>
-              Explore and enjoy from a list of Movies collection.
-            </p>
-            <p>
-              Start with this one....
-            </p>
+            <h1 className="banner__intro__title">Welcome to MoviesWorld!</h1>
+            <p>Explore and enjoy from a list of Movies collection.</p>
+            <p>Start with this one....</p>
             <div className="showInfo__summary">
-              After a violent shipwreck, billionaire playboy Oliver
-              Queen was missing and presumed dead for five years before
-              being discovered alive on a remote island in the Pacific
-              . He returned home to Starling City, welcomed by his devoted mother Moira
-              , beloved sister Thea and former flame Laurel Lance.
-              With the aid of his trusted chauffeur/bodyguard John Diggle
-              , the computer-hacking skills of Felicity Smoak and the occasional
-              , reluctant assistance of former police detective, now beat cop
-              , Quentin Lance, Oliver has been waging a one-man war on crime.
+              Hellsing is a 13-part anime based on the manga of the same name.
+              The plot is significantly difficult to that of the the manga
+              although the characters are the same. The show mainly focuses on
+              the hellsing institute that houses the anti-hero named Alucard who
+              swore loyalty to the Helsing family many years before. Alucard
+              being the first ever vampire takes on a new apprentice named
+              Sera's.
             </div>
-            <h4 className="tagline">
-              Find next? ...
-            </h4>
-            <h5 className="appName">
-            MoviesWorld!
-            </h5>
+            <h4 className="tagline">Find next? ...</h4>
+            <h5 className="appName">MoviesWorld!</h5>
           </article>
         </div>
       </section>
@@ -74,28 +60,31 @@ const Shows = () => {
         </form>
       </section>
       <main className="shows__container">
-        {shows.filter((show) => {
-          if (search === '') {
-            return true;
-          } if (show.name.toLowerCase().includes(search.toLowerCase())) {
-            return true;
-          } return false;
-        }).map((show) => (
-          <IconButton
-            key={show.id}
-            onClick={() => navigate(`/showInfos/${show.id}`)}
-            className="icon__button--noDeco"
-          >
-            <Show
-              id={show.id}
-              name={show.name}
-              image={show.image.medium}
-              rating={show.rating.average}
-              genres={show.genres[0]}
-            />
-          </IconButton>
-
-        ))}
+        {shows
+          .filter((show) => {
+            if (search === "") {
+              return true;
+            }
+            if (show.name.toLowerCase().includes(search.toLowerCase())) {
+              return true;
+            }
+            return false;
+          })
+          .map((show) => (
+            <IconButton
+              key={show.id}
+              onClick={() => navigate(`/showInfos/${show.id}`)}
+              className="icon__button--noDeco"
+            >
+              <Show
+                id={show.id}
+                name={show.name}
+                image={show.image.medium}
+                rating={show.rating.average}
+                genres={show.genres[0]}
+              />
+            </IconButton>
+          ))}
       </main>
     </div>
   );
